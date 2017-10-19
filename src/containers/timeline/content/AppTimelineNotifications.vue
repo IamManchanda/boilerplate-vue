35b708e618e838c7d553f7cb6f1781e7e74d9f06
harry <harmanmanchanda182@gmail.com>
2017-10-19 15:42:55 +0530

    Add sub routes!

---
+++ src/containers/timeline/content/AppTimelineNotifications.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="h5">Notifications</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum commodi consequuntur quidem impedit soluta doloribus ab officia quia beatae deserunt sed at vitae sapiente eos quam quibusdam nisi tempore fuga quisquam est reiciendis, officiis illum omnis mollitia aut. Vero tenetur tempora pariatur est, nulla quas expedita illo aut vitae ex, deserunt, eligendi ea consectetur ab! Amet consequuntur labore dignissimos nam! Voluptas incidunt corporis sed molestias quam ipsa ea, cupiditate, id voluptate soluta minima labore architecto aliquam! Ipsa assumenda mollitia saepe libero ipsum, beatae tenetur sed magnam labore, deleniti consequuntur sequi quibusdam natus obcaecati sint. Ipsum vitae, odio quo hic cupiditate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero dolorum accusamus incidunt eveniet dolorem reprehenderit libero obcaecati ad corporis adipisci blanditiis similique natus laborum, corrupti suscipit.</p>
+  </div>
+</template>
---
+++ src/containers/timeline/sidebar/AppTimelineSidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <router-link :to="{ name: 'timeline-home' }" tag="li" class="list-group-item" active-class="active" exact>
+        Timeline Home
+      </router-link>
+      <router-link :to="{ name: 'timeline-account' }" tag="li" class="list-group-item" active-class="active">
+        Account Settings
+      </router-link>
+      <router-link :to="{ name: 'timeline-messages' }" tag="li" class="list-group-item" active-class="active">
+        Messages
+      </router-link>
+      <router-link :to="{ name: 'timeline-notifications' }" tag="li" class="list-group-item" active-class="active">
+        Notifications
+      </router-link>
+    </ul>
+  </div>
+</template>
